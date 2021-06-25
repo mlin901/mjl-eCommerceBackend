72484033c9c69243ced05ae908ead3aba94af849
Matt Linder <mjlinder218@gmail.com>
2021-06-25 07:26:48 -0700

    Added the first two routs: '/' and '/:id'

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,39 @@
 const router = require('express').Router();
+// const sequelize = require('../../config/connection');  // **** May need to add this
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+// find all products
+// be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll();
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+    include: [{ all: true, nested: true }]  // From https://stackoverflow.com/questions/46614290/sequelize-eager-loading-error
+      // include: [{ model: Tag, through: ProductTag, as: 'Product-Tag' }]  // ********Didn't work, and didn't include category. Remove.
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id.' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
