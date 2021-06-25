5b87c4af74c0ce9c9348a5a9ea5b5cb9b996ad02
Matt Linder <mjlinder218@gmail.com>
2021-06-25 11:05:05 -0700

    Improved  as:... names

--- models/index.js
+++ models/index.js
@@ -23,7 +23,7 @@ Product.belongsToMany(Tag, {
     unique: false
   },
   // Define an alias for when data is retrieved
-  as: 'product_to_tags'
+  as: 'product_tags'
 });
 
 // Tags belongToMany Products (through ProductTag)
@@ -34,7 +34,7 @@ Tag.belongsToMany(Product, {
     unique: false
   },
   // Define an alias for when data is retrieved
-  as: 'tag_to_prodcts'
+  as: 'tag_products'
 });
 
 module.exports = {
