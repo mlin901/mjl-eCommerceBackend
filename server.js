6cc93c934134d0342319b1a6a6f9528f773d93b5
Matt Linder <mjlinder218@gmail.com>
2021-06-27 19:53:39 -0700

    Changed port number

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;    // *********3001
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,5 +13,5 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}.`));
 });
